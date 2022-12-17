a15063bbb834bcf6a7e895e62c8241656a7b8f18
Princehacker525 <106022397+Princehacker525@users.noreply.github.com>
2022-12-17 16:04:41 +0530

    Update pyaternos.py

--- pyaternos.py
+++ pyaternos.py
@@ -1,42 +1,8 @@
+from flask import Flask, render_template
 
-import requests
-import json
-def login(mail, password):
+app = Flask(__name__)
 
-s = requests.Session()
 
-payload={
-
-'email': mail,
-
-'password': password
-
-}
-
-res = s.post('https://aternos.org/', json-payload)
-
-s.headers.update({'authorization': json.loads(res.content)['token']})
-
-print(res.content)
-
-return s
-
-session = login('Lagjavacrash', 'trewq12345')
-
-r = session.patch('https://aternos.org/account/', json={'username'}) print(r.content)
-from selenium import webdriver
-
-from webdriver_manager.chrome import Chrome DriverManager
-import time
-
-browser = webdriver.Chrome (ChromeDriverManager().install())
-
-browser.get('https://aternos.org/')
-
-time.sleep(5)
-
-button = browser.find_element_by_link_text('Start')
-
-button.click()
-
-time.sleep(3)
+@app.route('/')
+def hello():
+    return render_template('index.html')
